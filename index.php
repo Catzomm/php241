bee9a263beaea207b091a93f6a6a55d069731a00
Artoym4523 <artpogud-info@yandex.ru>
2025-02-18 12:57:07 +0300

    new commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Погудин Артём 321</title>
+</head>
+<body>
+    <?php
+    echo "<p>Hello, World!</p>"
+    ?>
+</body>
+</html>
