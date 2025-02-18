b57911ec4f381143a63f7db34456cdc0631a1f52
Catzomm <artpogud@gmail.com>
2025-02-18 17:38:44 +0300

    new commit2

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Погудин Артём 321</title>
+    <title>Погудин Артёмм 321</title>
 </head>
 <body>
     <?php
