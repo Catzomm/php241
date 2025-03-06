fc68953e365bd52575bc08542f4ee1b5cd103533
Catzomm <artpogud@gmail.com>
2025-03-06 19:26:54 +0300

    Сделал первую лабу

--- Lab1/index.php
+++ Lab1/index.php
@@ -2,11 +2,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Погудин Артёмм 321</title>
+    <title>Погудин Артём</title>
+    <link rel="stylesheet" href="/style.css">
 </head>
 <body>
-    <?php
-    echo "<p>Hello, World!</p>"
-    ?>
+    <header class="header">
+        <img class="logo" src="./img/logo.jpg" alt="Mospolytech">
+        <h1 class="headline">2.1.Домашняя работа: Hello, World!</h1>
+    </header>
+    <p class="text">
+        <?php
+            echo "Hello, World!"
+        ?>
+    </p>
+    <footer class="footer">Создать веб-страницу с динамическим контентом. Загрузить код в удаленный репозиторий.</footer>
 </body>
 </html>
